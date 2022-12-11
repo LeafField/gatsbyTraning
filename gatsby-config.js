a705536e3503f8b88735cf11b147d4691460b3f7
LeafField <109466326+LeafField@users.noreply.github.com>
2022-12-11 21:54:48 +0900

    imgタグをgatsby-imageに変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@ module.exports = {
     title: `MySite`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+      }
+    }
+  ]
 };
